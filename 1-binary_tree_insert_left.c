3f5fa0dbd6d04e99a80e0fd3e71448cb42d94392
EliBrank <eliasbrinkman@gmail.com>
2024-04-14 18:12:37 -0500

    setup insert left func

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,11 +3,37 @@
 /**
  * binary_tree_insert_left - inserts node as left child of another node
  * @parent: pointer to parent, to insert left child in
- * @value: value to assign to new node
+ * @value: value to assign to new_left node
  *
  * Return: pointer to created node, NULL if failure/parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_left = NULL;
 
+	/* MALLOC ALERT */
+	new_left = malloc(sizeof(binary_tree_t));
+
+	if (new_left == NULL)
+	{
+		return (NULL);
+	}
+
+	new_left->n = value;
+	new_left->left = NULL;
+	new_left->right = NULL;
+	new_left->parent = NULL;
+
+	if (parent != NULL)
+	{
+		new_left->parent = parent;
+		if (parent->left != NULL)
+		{
+			new_left->left = parent->left;
+			new_left->left->parent = new_left;
+			parent->left = new_left;
+		}
+	}
+
+	return (new_left);
 }
