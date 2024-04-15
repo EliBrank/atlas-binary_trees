3c029fdba53a5a3a78e6b05689d925df518f6b39
EliBrank <eliasbrinkman@gmail.com>
2024-04-15 15:44:42 -0500

    correct betty formatting for is_root func

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if node is a leaf
+ * binary_tree_is_root - checks if node is root
  * @node: pointer to node to check
  *
- * Return: 1 if node is a leaf, else 0
+ * Return: 1 if node is root, else 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
@@ -12,7 +12,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	
+
 	if (node->parent == NULL)
 	{
 		return (1);
