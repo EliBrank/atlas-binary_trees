cfff1548073a73325f79bbd55080eb5494a94f08
EliBrank <eliasbrinkman@gmail.com>
2024-04-15 22:00:34 -0500

    work on non-leaf node count func, start sibling func

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,5 +8,21 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	
+
+	return (binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right));
 }
