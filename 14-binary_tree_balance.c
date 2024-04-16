76b610171dae17b459f645ee9b60de2abcfcc5f6
EliBrank <eliasbrinkman@gmail.com>
2024-04-16 00:26:03 -0500

    use height func to configure balance func

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,5 +8,33 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t h_left = 0, h_right = 0;
 
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+
+	return (h_left - h_right);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of tree to measure
+ *
+ * Return: height of binary tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left = 0, h_right = 0;
+
+	/* height is 0 in case of single node or empty tree */
+	if (tree == NULL)
+		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+
+	if (h_left > h_right)
+		return (1 + h_left);
+	else
+		return (1 + h_right);
 }
