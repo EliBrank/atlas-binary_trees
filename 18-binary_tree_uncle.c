d5b4fc37eef0079ead81308c6946ededce07a1f1
EliBrank <eliasbrinkman@gmail.com>
2024-04-16 01:31:26 -0500

    use sibling func to create uncle func

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,5 +8,28 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL ||
+		node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
 
+	return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling - finds sibling of node
+ * @node: pointer to node to check for sibling
+ *
+ * Return: pointer to sibling node, NULL if failure or parent/node is NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
