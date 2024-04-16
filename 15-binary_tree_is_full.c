c5db1100aa68a78f97a7068bb81dd1e710b962be
EliBrank <eliasbrinkman@gmail.com>
2024-04-16 00:59:01 -0500

    make func to check for single child nodes for is_full func

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+int half_node_check(const binary_tree_t *tree);
+
 /**
  * binary_tree_is_full - checks if binary tree is full
  * @tree: pointer to the root node of tree to check
@@ -8,5 +10,39 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
+	if (half_node_check(tree) == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
+
+/**
+ * half_node_check - checks tree for nodes with a single child
+ * @tree: pointer to the root node of tree to check
+ *
+ * Return: number of nodes with single child
+ */
+int half_node_check(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left == NULL && tree->right != NULL) ||
+		(tree->left != NULL && tree->right == NULL))
+	{
+		return (1);
+	}
+
+	return (half_node_check(tree->left) +
+	half_node_check(tree->right));
 }
