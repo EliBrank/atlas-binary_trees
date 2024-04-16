db33a2b307b4f558dce164114d0c82d6bc013aec
EliBrank <eliasbrinkman@gmail.com>
2024-04-16 00:14:50 -0500

    rework non-leaf node func

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,16 +13,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left != NULL || tree->right != NULL)
-	{
-		return (1);
-	}
-	else
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (0);
 	}
-	
 
-	return (binary_tree_nodes(tree->left) +
+	return (1 + binary_tree_nodes(tree->left) +
 			binary_tree_nodes(tree->right));
 }
